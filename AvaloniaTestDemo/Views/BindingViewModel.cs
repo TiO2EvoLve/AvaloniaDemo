22001ce39933c0950e46be4ae46b7e5a4e1da44d
TiO2EvoLve <49835162+TiO2EvoLve@users.noreply.github.com>
2026-08-18 08:45:54 +0800

    20260818

--- AvaloniaTestDemo/Views/BindingViewModel.cs
+++ AvaloniaTestDemo/Views/BindingViewModel.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
+using System.Globalization;
+using Avalonia.Data.Converters;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using Material.Icons;
 
 namespace AvaloniaTestDemo.Views;
@@ -11,16 +11,14 @@ namespace AvaloniaTestDemo.Views;
 public partial class BindingViewModel() :  DemoPageBase("绑定", MaterialIconKind.LinkVariant)
 {
     //滑动条绑定数字
-    [ObservableProperty]
-    public partial int Number { get; set; } = 0;
+    [ObservableProperty] private int number;
 
     // 下拉框当前选择的项
     [ObservableProperty]
     public partial Option SelectedSeason { get; set; }
 
     // 枚举列表，下拉框绑定
-    public IEnumerable<Option> Season { get; }
-        = Enum.GetValues<Option>();
+    public IEnumerable<Option> Season { get; } = Enum.GetValues<Option>();
     //枚举
     public enum Option
     {
@@ -36,5 +34,7 @@ public partial class BindingViewModel() :  DemoPageBase("绑定", MaterialIconKi
         [Description("冬天")]
         Winter
     }
-    
+    //checkbox绑定
+    [ObservableProperty]
+    private bool isCheck;
 }
